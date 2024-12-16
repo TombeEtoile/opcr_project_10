1f0eaf2b49e1916616c08611cc7b9f0bcc525f74
Augustin <aug.verdier@gmail.com>
2024-12-16 11:35:32 +0100

    add 'scraping_url' file

---
+++ exercices_coding_game/linkedin.py
@@ -0,0 +1,7 @@
+print("lkjsfd")
+
+print("lkjsfd")
+
+def print_hello():
+
+    print("Hello LinkedIn")
---
+++ exercices_coding_game/test_1.py
@@ -0,0 +1,17 @@
+n = int(input('Number : ').strip())
+
+if n % 2 == 1:
+    print("Weird")
+
+elif 2 < n < 5:
+    print("Not weird")
+
+elif 6 < n < 20:
+    print('weird')
+
+else:
+    print('Not weird')
+
+
+
+
